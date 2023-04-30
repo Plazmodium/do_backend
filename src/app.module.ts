c51a77e27de4d38b51e73de37aed57264699e24f
Gabor Racz <gabzracz.dev@gmail.com>
2023-04-30 17:00:05 +0200

    setup of jetbrains space dev environments

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({
+    isGlobal: true,
+    envFilePath: `${process.env.NODE_ENV}.json`
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
